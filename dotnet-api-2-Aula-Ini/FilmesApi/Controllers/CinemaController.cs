84e4b5e50ed8c2141fec23780be54c980437f13b
LeCaVeiRa <39318329+LeCaVeiRa@users.noreply.github.com>
2023-06-01 17:19:27 -0300

    relacao de filme e cinema; consultas em sql

--- dotnet-api-2-Aula-Ini/FilmesApi/Controllers/CinemaController.cs
+++ dotnet-api-2-Aula-Ini/FilmesApi/Controllers/CinemaController.cs
@@ -3,6 +3,8 @@ using FilmesApi.Data;
 using FilmesApi.Data.Dtos;
 using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace FilmesApi.Controllers;
 
@@ -23,9 +25,14 @@ public class CinemaController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<ReadCinemaDto> RecuperaCinemas()
+    public IEnumerable<ReadCinemaDto> RecuperaCinemas([FromQuery] int? enderecoId = null)
     {
-        return _mapper.Map<List<ReadCinemaDto>>(_context.Cinemas.ToList());
+        if(enderecoId == null)
+        {
+            return _mapper.Map<List<ReadCinemaDto>>(_context.Cinemas.ToList());
+        }
+        return _mapper.Map<List<ReadCinemaDto>>
+            (_context.Cinemas.FromSqlRaw($"SELECT Id, Nome, EnderecoId FROM cinemas WHERE cinemas.EnderecoId = {enderecoId}").ToList());
     }
 
     [HttpGet("{id}")]
